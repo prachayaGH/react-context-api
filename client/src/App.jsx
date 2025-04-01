bf421d9df3012441e75b0aeb978cc18343f67026
Suriya <true.many.032@gmail.com>
2025-04-01 16:14:46 +0700

    Assignment 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+import React, { createContext } from "react";
 
+export const UserContext = createContext();
 function App() {
   const userData = {
     username: "John",
@@ -13,12 +15,14 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <UserContext.Provider value={userData}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../App";
 
 function ViewProductPage() {
   const navigate = useNavigate();
+  const userData = useContext(UserContext);
   return (
     <div>
       <h1>View Product Page</h1>
@@ -11,7 +14,7 @@ function ViewProductPage() {
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <h2>คุณคือสมาชิกในระดับ {userData.level} ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
         <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
